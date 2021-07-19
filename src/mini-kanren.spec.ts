a722369240616e7cf4b01905f74ac9a65b3dc09a
Erik Baklund <erik@hubii.com>
2021-07-19 17:20:13 +0200

    Added type checks

--- src/mini-kanren.spec.ts
+++ src/mini-kanren.spec.ts
@@ -1,5 +1,4 @@
 
-
 import * as chai from 'chai';
 import * as chaiAsPromised from 'chai-as-promised';
 
@@ -8,9 +7,50 @@ chai.use(chaiAsPromised);
 import * as mk from './mini-kanren';
 
 const { expect } = chai;
+const given = describe;
+const when = describe;
 
-describe ('js-kanren', () => {
-  it ('hello', () => {
-    expect(false).to.equal(true);
+const N = 20000000;
+
+describe ('js-kanren type recognition', () => {
+  given ('an object', () => {
+    when ('checking its type', () => {
+      it ('recognizes strings', () => {
+        expect(mk.isString("")).to.be.true;
+        expect(mk.isString(String(""))).to.be.true;
+        expect(mk.isString(0)).to.be.false;
+      });
+
+      it ('recognizes numbers', () => {
+        expect(mk.isNumber(0)).to.be.true;
+        expect(mk.isNumber("")).to.be.false;
+      });
+
+      it ('recognizes arrays', () => {
+        expect(mk.isArray([])).to.be.true;
+        expect(mk.isArray("")).to.be.false;
+      });
+
+      it ('recognizes functions', () => {
+        expect(mk.isFunction(() => 0)).to.be.true;
+        expect(mk.isFunction("")).to.be.false;
+      });
+
+      it ('recognizes objects', () => {
+        expect(mk.isObject({})).to.be.true;
+        expect(mk.isObject(null)).to.be.false;
+        expect(mk.isObject(String(""))).to.be.false;
+      });
+
+      it ('recognizes null', () => {
+        expect(mk.isNull(null)).to.be.true;
+        expect(mk.isNull({})).to.be.false;
+      });
+
+      it ('recognizes undefined', () => {
+        expect(mk.isUndefined(undefined)).to.be.true;
+        expect(mk.isUndefined({})).to.be.false;
+      });
+    });
   });
 });
--- src/mini-kanren.ts
+++ src/mini-kanren.ts
@@ -1,1 +1,30 @@
 
+// Type recognition
+
+export function isString(u: unknown): boolean {
+  return Object.prototype.toString.call(u) === '[object String]';
+}
+
+export function isNumber(u: unknown): boolean {
+  return Object.prototype.toString.call(u) === '[object Number]';
+}
+
+export function isFunction(u: unknown): boolean {
+  return Object.prototype.toString.call(u) === '[object Function]';
+}
+
+export function isArray(u: unknown): boolean {
+  return Object.prototype.toString.call(u) === '[object Array]';
+}
+
+export function isObject(u: unknown): boolean {
+  return Object.prototype.toString.call(u) === '[object Object]';
+}
+
+export function isNull(u: unknown): boolean {
+  return u === null;
+}
+
+export function isUndefined(u: unknown): boolean {
+  return u === undefined;
+}
